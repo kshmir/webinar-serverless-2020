74fe5522a7bac8e9d98ba9c96671abb6f1f9cd43
Cristian Pereyra <cristian@innovid.com>
2020-05-05 11:51:37 -0700

    add code for production deployment

--- webinar-chrome/handler.js
+++ webinar-chrome/handler.js
@@ -3,7 +3,7 @@ const chromium = require('chrome-aws-lambda');
 module.exports.urlImage = async (event) => {
   const { url } = event.queryStringParameters;
   const browser = await chromium.puppeteer.launch({
-    executablePath: await chromium.executablePath,
+    executablePath: await chromium.executablePath, args: chromium.args,
     defaultViewport: { height: 800, width: 1280 }, headless: true,
   });
   const page = await browser.newPage();
